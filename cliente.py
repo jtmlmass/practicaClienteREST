0dc5ec375459f7300f4ccb0132f404462773477a
Tomas Molina <jtml.mass@gmail.com>
2019-11-29 15:15:09 -0400

    Agregar get con soporte de retornar el estudiante y matricula en constructor

--- cliente.py
+++ cliente.py
@@ -3,10 +3,16 @@ import json
 
 
 class Estudiante:
-    def __init__(self, nombre, correo, carrera):
+    def __init__(self, nombre, correo, carrera, matricula=0):
         self.nombre = nombre
         self.correo = correo
         self.carrera = carrera
+        self.matricula = matricula
+
+    def __str__(self):
+        estudiante = "Nombre: " + str(self.nombre) + "\nMatricula: " + str(
+            self.matricula) + "\nCorreo: " + str(self.correo) + "\nCarrera: " + str(self.carrera)
+        return estudiante
 
 
 def jprint(obj):
@@ -33,7 +39,7 @@ def get_estudiantes():
 
 
 def post_estudiante():
-    nombre = input("Inserte nombre del estudiante: ")
+    nombre = input("\nInserte nombre del estudiante: ")
     correo = input("Inserte correo del estudiante: ")
     carrera = input("Inserte carrera del estudiante: ")
     estudiante = Estudiante(nombre, correo, carrera)
@@ -50,7 +56,10 @@ def get_estudiante():
     matricula = int(input("Inserte la matricula del estudiante: "))
     response = requests.get(
         "http://localhost:4567/rest/estudiantes/" + str(matricula))
-    print(response.json())
+    estudiante = Estudiante(
+        response.json()["nombre"], response.json()["correo"], response.json()["carrera"], response.json()["matricula"])
+    print("\n" + str(estudiante))
+    return estudiante
 
 
 opcion = -1
